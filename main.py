7e90d7da953581608fb80b7b5ac735f1c83950f2
Jeremy Malloch <jeremalloch@gmail.com>
2016-06-04 21:56:54 -0400

    Latest changes

--- main.py
+++ main.py
@@ -16,17 +16,24 @@ import matplotlib.animation as animation
 import numpy as np
 
 #Take in from command line the refresh rate of data coming in so it will
-#be used to match the refresh rate of the animation
+#be used to match the refresh rate of the animation (in milliseconds)
 sys.argv[1] = refreshRate
 
-#Take in from command line the length of time of the window to view in seconds
+#Take in from command line the length of time of the window to view in milliseconds
 sys.argv[2] = frameLength
+
+#Global variable for the number of samples that will be displayed at any time
+numSamples = frameLength*refreshRate
 
 #Global variable for the number of data channels coming (number of plots is 
 #double this, since there is the filtered and unfiltered output)
 inChannels = 2
 
 #Initialize the Pandas Dataframe that will store all the data
+df = pd.DataFrame(np.random.randn(10, 4))
+
+#Initialize the x-axis by using the refresh rate and window view length
+xAxis = np.linspace(0.0, num=(numSamples+1), endpoint=False)
 
 #Initilize lists of the subplots that
 unFilteredPlots = []
@@ -36,8 +43,6 @@ for channels in range(inChannels):
     unFilteredPlots.append(plt.figure())
     FilteredPlots.append(plt.figure())
     
-
-fig = plt.figure()
 unfiltered1 = fig.add_subplot(2,2,1)
 
 
