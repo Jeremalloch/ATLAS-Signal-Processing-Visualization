d98fc967c558b58b73798545c332376049c1cbb4
Jeremy Malloch <jeremalloch@gmail.com>
2016-06-06 13:57:22 -0400

    Began debugging Window class

--- main.py
+++ main.py
@@ -50,17 +50,22 @@ class Current_Data(object):
 		self.inChannels = inChannels_
 		self.df = pd.DataFrame(np.zeros((numSamples_, inChannels_*2)), columns=Columns)
 		#Log the results in a CSV file with current day and time as file name
-		self.fileName = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		self.fileName = strftime("%Y-%m-%d %H:%M:%S", gmtime())+'.csv'
+		#Try out sys.path() so that CSV file is saved in same location as program
 		self.df.to_csv(self.fileName)
-		with open('foo.csv', 'a') as self.f:
+		with open(str(self.fileName), 'a') as self.f:
                     self.writer = csv.writer(self.f)
 
 	def update(self, newRow):
-		#self.writer.writerows(newRow)
-		self.df.loc[-1] = newRow  #Adding the new row
-                self.df.index = self.df.index + 1  #Shifting the row index up by one
-                self.df = self.df.sort_index()  #Sorting the dataframe by index
-                self.df.drop(10)  #Need to improve the drop function
+	    '''
+	    Update the dataframe object, removing the oldest row entry, and 
+	    adding the newRow iterable to the top of the dataframe
+	    '''
+            #self.writer.writerows(newRow)
+            self.df.loc[-1] = newRow  #Adding the new row
+            self.df.index = self.df.index + 1  #Shifting the row index up by one
+            self.df = self.df.sort_index()  #Sorting the dataframe by index
+            self.df.drop(self.df.index[10], inplace=True)  #Drop the oldest data from the dataframe
 
 	def unFiltData(self, channel_number):
 		'''
@@ -75,16 +80,23 @@ class Current_Data(object):
 		the channel number passed as a parameter
 		'''
 		return self.df[('Filt_'+str(channel_number))]
+        def test(self):
+            '''
+            Test function that calls update rows, fills in rows with for
+            loop to test function of update function
+            '''
+            for x in range(1,15):
+                self.update([x,x,x,x])
+            return self.df
+        
 
 ##Initilize lists of the subplots that
 #unFilteredPlots = []
 #FilteredPlots = []
 #
-#Initialize Current_Data Object
-Data = Current_Data(numSamples, inChannels)
+
 #
-##Initializing the plot
-#fig = plt.figure()
+
 #
 ##Initialize all the subplots
 #for channels in range(inChannels):
@@ -99,32 +111,40 @@ Data = Current_Data(numSamples, inChannels)
 
 
 
-#class Window(object):
-#	'''
-#	Class that generates the figure to plot
-#	'''
-#    def __init__(self, fig_):
-#    	self.fig = fig_
-#    	self.xAxis = [-1.0*refreshRate*x for x in range(numSamples)]
-#    	self.unFilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels)) for channels in range(inChannels)]
-#    	self.FilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels+1)) for channels in range(inChannels)]
-#    	#Add label of each column (Unfiltered vs filtered)
-#    	self.unFilteredPlots[0].set_title('Unfiltered Data')
-#		self.FilteredPlots[0].set_title('Filtered Data')
-#
-#    def __call__(self, i):
-#        # This way the plot can continuously run and we just keep
-#        # watching new realizations of the process
-#        if i == 0:
-#            return self.init()
-#        for subplot in self.unFilteredPlots:
-#        	unFiltTemp.plot(self.xAxis, Data.unFiltData(channels))
-#        for subplot in self.FilteredPlots:
-#        	FiltTemp.plot(self.xAxis, Data.FiltData(channels))
-#        return self.fig
-#
-##Label the x-axis 
-#plt.xlabel('time (ms)')
-#
-#frame1 = animation.TimedAnimation(fig, interval=refreshRate)
-#plt.show()
+class Window(object):
+    '''
+    Class that generates the figure to plot
+    '''
+    def __init__(self, fig_):
+        self.fig = fig_
+        self.xAxis = [-1.0*refreshRate*x for x in range(numSamples)]
+        self.unFilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels)) for channels in range(inChannels)]
+        self.FilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels+1)) for channels in range(inChannels)]
+        #Add label of each column (Unfiltered vs filtered)
+        self.unFilteredPlots[0].set_title('Unfiltered Data')
+        self.FilteredPlots[0].set_title('Filtered Data')
+
+    def __call__(self, i):
+        # This way the plot can continuously run and we just keep
+        # watching new realizations of the process
+        if i == 0:
+            return self.init()
+        for subplot in self.unFilteredPlots:
+        	unFiltTemp.plot(self.xAxis, Data.unFiltData(channels))
+        for subplot in self.FilteredPlots:
+        	FiltTemp.plot(self.xAxis, Data.FiltData(channels))
+        return self.fig
+
+if __name___ == '__main__':
+    #Initializing the plot
+    fig = plt.figure()
+    
+    #Initialize Current_Data Object
+    Data = Current_Data(numSamples, inChannels)
+    #Initialize the window object
+    window = Window(fig)
+    #Label the x-axis 
+    plt.xlabel('time (ms)')
+    
+    #frame1 = animation.TimedAnimation(fig, interval=refreshRate)
+    #plt.show()
