d7958a728920729a1d24bb58637c5967e5f8895e
Jeremy Malloch <jeremalloch@gmail.com>
2016-06-09 17:03:39 -0400

    Trying PyCharm

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python
-
-__author__ = "Jeremy Malloch"
-__status__ = "Prototype"
-
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-#import seaborn as sns
+import seaborn as sns
 from time import gmtime, strftime
 import csv
 import string
 
+__author__ = "Jeremy Malloch"
+__status__ = "Prototype"
+
 #Kinda obvious who wrote the program based on the style haha
-#plt.style.use('seaborn-colorblind')
+plt.style.use('seaborn-colorblind')
 
 #Take in from command line the refresh rate of data coming in so it will
 #be used to match the refresh rate of the animation (in milliseconds)
@@ -39,7 +37,7 @@ inChannels = 2
 #Initialize the x-axis by using the refresh rate and window view length
 xAxis = [-1.0*refreshRate*x for x in range(numSamples)]
 
-class Current_Data(object):
+class Current_Data:
 	"""Holds the most recent data that is currently displayed"""
 
 	def __init__(self, numSamples_, inChannels_):
@@ -54,13 +52,13 @@ class Current_Data(object):
 		#Try out sys.path() so that CSV file is saved in same location as program
 		self.df.to_csv(self.fileName)
 		with open(str(self.fileName), 'a') as self.f:
-                    self.writer = csv.writer(self.f)
+            self.writer = csv.writer(self.f)
 
 	def update(self, newRow):
-	    '''
-	    Update the dataframe object, removing the oldest row entry, and 
-	    adding the newRow iterable to the top of the dataframe
-	    '''
+        """
+        Update the dataframe object, removing the oldest row entry, and
+        adding the newRow iterable to the top of the dataframe
+        """
             #self.writer.writerows(newRow)
             self.df.loc[-1] = newRow  #Adding the new row
             self.df.index = self.df.index + 1  #Shifting the row index up by one
@@ -68,27 +66,28 @@ class Current_Data(object):
             self.df.drop(self.df.index[10], inplace=True)  #Drop the oldest data from the dataframe
 
 	def unFiltData(self, channel_number):
-		'''
-		Method that returns a list of the unfiltered data corresponding to
-		the channel number passed as a parameter
-		'''
-		return self.df[('UnFilt_'+str(channel_number))]
+        """
+        Method that returns a list of the unfiltered data corresponding to
+        the channel number passed as a parameter
+        """
+	    return self.df[('UnFilt_'+str(channel_number))]
 
 	def FiltData(self, channel_number):
-		'''
-		Method that returns a list of the filtered data corresponding to
-		the channel number passed as a parameter
-		'''
+        """
+        Method that returns a list of the filtered data corresponding to
+        the channel number passed as a parameter
+        """
 		return self.df[('Filt_'+str(channel_number))]
-        def test(self):
-            '''
-            Test function that calls update rows, fills in rows with for
-            loop to test function of update function
-            '''
-            for x in range(1,15):
-                self.update([x,x,x,x])
-            return self.df
-        
+
+    def test(self):
+        """
+        Test function that calls update rows, fills in rows with for
+        loop to test function of update function
+        """
+        for x in range(1,15):
+            self.update([x,x,x,x])
+        return self.df
+
 
 ##Initilize lists of the subplots that
 #unFilteredPlots = []
@@ -111,31 +110,36 @@ class Current_Data(object):
 
 
 
-class Window(object):
-    '''
+class Window(Current_Data:
+    """
     Class that generates the figure to plot
-    '''
+    """
     def __init__(self, fig_):
+        super().__init__()
         self.fig = fig_
-        self.xAxis = [-1.0*refreshRate*x for x in range(numSamples)]
+        self.xAxis = [-1.0*refreshRate*x for x in range(numSamples+1)]
         self.unFilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels)) for channels in range(inChannels)]
+        
         self.FilteredPlots = [(self.fig.add_subplot(inChannels,2,1 + 2*channels+1)) for channels in range(inChannels)]
         #Add label of each column (Unfiltered vs filtered)
         self.unFilteredPlots[0].set_title('Unfiltered Data')
         self.FilteredPlots[0].set_title('Filtered Data')
 
     def __call__(self, i):
-        # This way the plot can continuously run and we just keep
-        # watching new realizations of the process
+        """
+        Plot can continuously run and we just keep
+        watching new realizations of the process
+        """
         if i == 0:
             return self.init()
         for subplot in self.unFilteredPlots:
         	unFiltTemp.plot(self.xAxis, Data.unFiltData(channels))
         for subplot in self.FilteredPlots:
         	FiltTemp.plot(self.xAxis, Data.FiltData(channels))
-        return self.fig
+        return self.figs
 
-if __name___ == '__main__':
+
+if __name__ == '__main__':
     #Initializing the plot
     fig = plt.figure()
     
