49f416ea0b876c9ba757452ab7c0419734ed336d
Jeremy Malloch <jeremalloch@gmail.com>
2016-06-22 11:41:53 -0400

    Debugged code, now trying to avoid using matploblib animation in order to make animation faster, allow concurrent read in of data

--- src/main.py
+++ src/main.py
@@ -48,19 +48,20 @@ class Data_Container:
     """
     Holds the most recent data that is currently displayed
     """
-    def __init__(self, inChannels_, frameLength_):
+    def __init__(self, inChannels_, frameLength_, dataInterval_):
+        self.dataInterval = dataInterval_
         self.inChannels = inChannels_
         self.frameLength = frameLength_
-        self.unFilterData = [deque(np.zeros(self.frameLength), maxlen=frameLength) for x in range(inChannels)]
-        self.FilteredData = [deque(np.zeros(self.frameLength), maxlen=frameLength) for x in range(inChannels)]
-        self.numSamples = 0
+        self.numSamples = self.frameLength // self.dataInterval
+        self.unFilterData = [deque(np.zeros(self.numSamples), maxlen=self.numSamples) for x in range(inChannels)]
+        self.FilteredData = [deque(np.zeros(self.numSamples), maxlen=self.numSamples) for x in range(inChannels)]
         Columns = [('UnFilt_' + str(x)) for x in range(1, inChannels + 1)]
         for x in range(1, inChannels + 1):
             Columns.append('Filt_' + str(x))
         # Initialize the CSV file using the columns as labels, then store the writer as an internal attribute
         # TODO look into putting the CSV file into the object
         # Log the results in a CSV file with current day and time as file name
-        # self.fileName = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '.csv'
+        self.fileName = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '.csv'
 
     def pullData(self):
         """
@@ -116,16 +117,19 @@ class Realtime_plot:
     Creates a Data_Container object, then takes data from that and updates the animation plot
     """
 
-    def __init__(self, plt, inChannels_):
-        self.data = Data_Container()
+    def __init__(self, plt, inChannels_, frameLength_, dataInterval_):
+        self.data = Data_Container(inChannels_, frameLength_, dataInterval_)
+        self.inChannels = inChannels_
         self.fig, self.PlotArray = plt.subplots(inChannels, 2, sharex='col', sharey='row', figsize = (12, 2 * inChannels))
-        self.lineArray = [self.PlotArray[0,x].plot(self.data.getUnfilteredData(x)), self.PlotArray[1,x].plot(self.data.getUnfilteredData(x))]
+        self.lineArray = []
+        for x in range(self.inChannels):
+            self.lineArray.append(self.PlotArray[x,0].plot(self.data.getUnfilteredData(x)))
+            self.lineArray.append(self.PlotArray[x,1].plot(self.data.getUnfilteredData(x)))
         self.xAxis = [-1*dataInterval * x for x in range(numSamples)]
         self.PlotArray[0, 0].set_title('Filtered Data')    #Add a label above the column of filtered plots
         self.PlotArray[0, 1].set_title('Unfiltered Data')    #Add a label above the column of unfiltered plots
         self.fig.text(0.3, 0.04, 'Time (ms)', ha='center', va='center') #Set an x-axis label for the first column
         self.fig.text(0.725, 0.04, 'Time (ms)', ha='center', va='center') #Set an x-axis label for the first column
-        self.inChannels = inChannels_
 
     def getPlot(self, Row, Column):
         """
@@ -150,13 +154,15 @@ class Realtime_plot:
         :return: matplotlib.figure.Figure
         """
         for num, row in enumerate(self.data.FilteredData):  # Update the unfiltered data plots
-            self.lineArray[num].plot(self.xAxis, self.data.getUnfilteredData(num))
-            self.lineArray[num + self.inChannels].plot(self.xAxis, self.data.getFiltereredData(num))
+            self.lineArray[num][0].set_ydata(self.data.getUnfilteredData(num))
+            self.lineArray[num + self.inChannels - 1][0].set_ydata(self.data.getFiltereredData(num))
         return self.lineArray
 
 # Initialize the window object
-display = Realtime_plot(plt)
+display = Realtime_plot(plt, inChannels, frameLength, dataInterval)
 
 fig = display.getFig()
 
-frame = animation.FuncAnimation(fig, Realtime_plot.updatePlot(), interval=displayInterval, blit=True)
+# display.updatePlot()
+
+frame = animation.FuncAnimation(fig, display.updatePlot(), interval=displayInterval, blit=True)
